11da62ae001f83ea97c3f4d1be6a96ec9952a96d
azat-bit <77639215+azat-bit@users.noreply.github.com>
2023-04-02 14:27:31 +0300

    Add files via upload

---
+++ İkinci_Dereceden_Denklemler.java
@@ -0,0 +1,14 @@
+public class İkinci_Dereceden_Denklemler {
+
+    public static void main(String args[]){
+
+
+        double a=2 ,b=3,c=15;
+
+        double determinat=b*b-4*a*c;
+        double kok1,kok2;
+
+        kok1 = (-b + Math.sqrt(determinat)) / (2 * a);
+        kok1 = (-b + Math.sqrt(determinat)) / (2 * a);
+    }
+}
